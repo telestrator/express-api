3751f956531e983b14ab1c013b152ea95316ac6d
George Bockari <bitwhys@gmail.com>
2024-07-27 20:15:23 -0400

    feat: add custom logger with winston and morgan

--- api/index.ts
+++ api/index.ts
@@ -1,12 +1,35 @@
 import express from 'express'
 import http from 'node:http'
-import logger from 'morgan'
+import morgan from 'morgan'
+import { logger } from '#logger'
+
+const morganFormat = ':method :url :status :response-time ms'
 
 const app = express()
 const PORT = process.env.PORT ?? 3000
 
 // Logger middleware
-app.use(logger('short'))
+app.use(
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-call -- morgan is a function
+  morgan(morganFormat, {
+    stream: {
+      write: message => {
+        const logObject = {
+          // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+          method: message.split(' ')[0],
+          // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+          url: message.split(' ')[1],
+          // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+          status: message.split(' ')[2],
+          // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+          responseTime: message.split(' ')[3],
+        }
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access,@typescript-eslint/no-unsafe-call -- logger.info is a function
+        logger.info(JSON.stringify(logObject))
+      },
+    },
+  })
+)
 
 app.use(function (req, res) {
   res.writeHead(200, { 'Content-Type': 'text/plain' })
